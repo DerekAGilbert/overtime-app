7d1485a06a502006fc6123f95254b8056f3d57b7
derekagilbert <dgibbbs17@gmail.com>
2017-05-24 15:11:08 -0600

    Implemented initial view code for audit logs

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -1,5 +1,28 @@
-# # spec/features/audit_log_spec
-#
-# require 'rails_helper'
-#
-# describe 'AuditLog Fearture' do
+# spec/features/audit_log_spec
+
+require 'rails_helper'
+
+describe 'AuditLog Fearture' do
+
+  describe 'index' do
+    before do
+         admin_user = FactoryGirl.create(:admin_user)
+         login_as(admin_user, :scope => :user)
+         FactoryGirl.create(:audit_log)
+     end
+     it 'has an index page that can be reached' do
+       visit audit_logs_path
+       expect(page.status_code).to eq(200)
+     end
+
+     it 'renders audit log content' do
+       visit audit_logs_path
+       expect(page).to have_content(/SNOW/)
+     end
+
+     xit ' cannot be accesed by non admin users' do
+
+     end
+   end
+
+end
