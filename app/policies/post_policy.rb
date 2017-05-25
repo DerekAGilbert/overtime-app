b56699a4862bd4f0769d4ba8f3e2dd20c43b77ad
derekagilbert <dgibbbs17@gmail.com>
2017-05-25 15:31:30 -0600

    Added permissions to admin user to approve posts

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,10 +1,15 @@
 class PostPolicy < ApplicationPolicy
 
+
   def update?
     return true if record.approved? && admin?
     return true if user_or_admin && !post_approved?
-
   end
+
+  def approve?
+    admin?
+  end
+
 
   private
 
