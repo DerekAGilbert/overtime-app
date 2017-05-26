8f4b81c1685c4ec23520d1c88c2da91e53400011
derekagilbert <dgibbbs17@gmail.com>
2017-05-26 15:41:31 -0600

    Employee homepage confirmations

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def homepage
-    if admin_types.include?(current_user.type) == 'AdminUser'
+    if admin_types.include?(current_user.type)
       @pending_approvals = Post.submitted
       @recent_audit_items = AuditLog.last(10)
     else
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -11,7 +11,21 @@ describe 'Homepage' do
 
     click_on ("approve_#{post.id}")
 
-    # expect(post.reload.status).to eq('approved')
+    expect(post.reload.status).to eq('approved')
 
+  end
+
+  it 'allows employee to change audit log statusfrom homepage' do
+    audit_log = FactoryGirl.create(:audit_log)
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    audit_log.update(user_id: user.id)
+
+    visit root_path
+
+    click_on ("confirm_#{audit_log.id}")
+
+    expect(audit_log.reload.status).to eq('confirmed')
   end
 end
